0c2e40331e6e49928fd03eaa0c45a80f1e8d792f
sajida-sheikh <sajidasheikh207@gmail.com>
2024-08-16 20:00:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+//-----------------------------welcome to the user-----------------------------//
+
+var username = prompt("Enter your Name ");
+var num = prompt("Enter your Roll no")
+alert("Welcome"+ " "+ username +" "+"Best of Luck for your Quiz!");
+
+//-----------------------------Question No:1-----------------------------//
+var ques1 = prompt("What does HTML stand for? A). Hyper text markup language, B). Hyper Links,  D). Home tool markup language ")
+var correct1 = "a"
+if(ques1 === correct1){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:2-----------------------------//
+var ques2 = prompt("Who is making the Web standards? A). Google, B). Mozilla, C). The World Wide Web Consortium, D). Microsoft")
+var correct2 = "c"
+if(ques2 === correct2){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:3-----------------------------//
+var ques3 = prompt("Choose the correct HTML element for the largest heading: A). <h1>, B). <heading>, C). <h6>, D). <head>")
+var correct3 = "a"
+if(ques3 === correct3){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:4-----------------------------//
+var ques4 = prompt("What is the correct HTML element for inserting a line break? A). <ib>, B). <br>, C). <rb>, D). <break>")
+var correct4 = "b"
+if(ques4 === correct4){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:5-----------------------------//
+var ques5 = prompt("Choose the correct HTML element to define important text. A). <b>, B). <important>, C). <i>, D). <strong>")
+var correct5 = "d"
+if(ques5 === correct5){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:6-----------------------------//
+var ques6 = prompt("Choose the correct HTML element to define emphasized text. A). <em>, B). <i>, C). <italic>, D). <strong>")
+var correct6 = "a"
+if(ques6 === correct6){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:7-----------------------------//
+var ques7 = prompt("Which character is used to indicate an end tag? A). *, B). ^, C). /, D). >")
+var correct7 = "c"
+if(ques7 === correct7){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:8-----------------------------//
+var ques8 = prompt("Inline elements are normally displayed without starting a new line. A). true, B). false")
+var correct8 = "b"
+if(ques8 === correct8){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:9-----------------------------//
+var ques9 = prompt("How can you make a numbered list? A).<list>, B). <ul>, C). <ol>, D).<dl>")
+var correct9 = "c"
+if(ques9 === correct9){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+//-----------------------------Question No:10-----------------------------//
+var ques10 = prompt("How can you make a bulleted list? A).<list>, B). <ul>, C). <ol>, D).<dl>")
+var correct10 = "b"
+if(ques10 === correct10){
+    alert("Correct Answer!")
+}
+else{
+    alert("Incorrect Answer!")
+}
+
+
+
+
+
+
+
+
